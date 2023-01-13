ccca246c33ba95ee1c93006b54dd58103f952c67
ben <johndoe@example.com>
2023-01-14 12:36:18 +1300

    comments added

--- src/components/NodeInput.js
+++ src/components/NodeInput.js
@@ -1,31 +1,33 @@
+/* NodeInput component contains main functionality for handling the input box  
+    and focusing on a node */
+
 export default function NodeInput({network}) {
 
-    function focusOnNode(node, network) {
+    // focus on node entered into input box
+    function onSubmitEvent(e) {
+        e.preventDefault();
+
         var options = {
             scale: 1.0,
             offset: { x: 0, y: -100 },
             animation: {
-              duration: 1000,
-              easingFunction: "easeInOutQuad",
+                duration: 1000,
+                easingFunction: "easeInOutQuad",
             },
         };
         
+        const node = (e.target)[0].value
         network.focus(node, options);
     }
-
-    function onSubmitEvent(e) {
-        e.preventDefault();
-
-        focusOnNode((e.target)[0].value, network);
-    }
     
+    // html for "focus on node" text input box
     return (
         <div style={{textAlign: "center"}}>
             <form data-testid='form' onSubmit={onSubmitEvent}>
-                <label> Enter question number to focus on:  </label>
+                <label>To centre a node, click on it or enter the number below: </label><br></br>
                 <input type='text' id='alert' name='alert' />
                 <button type='submit'> Enter </button>
             </form>
-      </div>
+        </div>
     )
-  }
+}
